00e7201b7a083d2711249955e7f9b846c6883e71
HotJuly <949588380@qq.com>
2022-02-20 16:55:48 +0800

    30.小程序数据请求

--- 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
+++ 1.小程序/02.代码/硅谷云音乐/pages/index/index.js
@@ -5,14 +5,46 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        // 该数组用于存储首页轮播图数据
+        banners:[]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        /*
+            1.在哪发
+                如果是Vue的话,一般就是created或者mounted
+                进入阶段的生命周期都可以发送,onLoad
 
+            2.怎么发
+                由于小程序并没有遵守W3C的规范,所以没有BOM和DOM
+                所以此处无法使用ajax发送请求
+
+                API:wx.request(Object object)
+
+            3.往哪发
+                看接口文档
+                请求三要素
+                    -请求方式
+                    -请求地址
+                    -请求参数
+        */
+       wx.request({
+           url:"http://localhost:3000/banner",
+           data:{
+               type:2
+           },
+           success:(res)=>{
+            //    此处返回的res是响应报文对象,其中包括了响应头和响应体数据
+            // 我们只需要响应体数据
+            // console.log(res.data)
+            this.setData({
+                banners:res.data.banners
+            })
+           }
+       })
     },
 
     /**
