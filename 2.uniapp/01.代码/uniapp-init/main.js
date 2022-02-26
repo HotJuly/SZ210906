a77f4b48c0669d374c93cbb8c69dc2e4445e6811
HotJuly <949588380@qq.com>
2022-02-26 16:25:59 +0800

    07.HBuilderX插件安装

--- 2.uniapp/01.代码/uniapp-init/main.js
+++ 2.uniapp/01.代码/uniapp-init/main.js
@@ -1,21 +1,23 @@
 import App from './App'
 
-// #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
+
+// 声明App组件代表整个项目,意思是代表小程序
 App.mpType = 'app'
+
+// 相当于在创建当前项目的实例对象,类似于小程序项目中的App()
 const app = new Vue({
     ...App
+	
+	// onLaunch: function() {
+	// 	console.log('App Launch')
+	// },
+	// onShow: function() {
+	// 	console.log('App Show')
+	// },
+	// onHide: function() {
+	// 	console.log('App Hide')
+	// }
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
--- 2.uniapp/01.代码/uniapp-init/pages/index/index.vue
+++ 2.uniapp/01.代码/uniapp-init/pages/index/index.vue
@@ -1,52 +1,73 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<!-- uniapp,即兼容html的标签,又兼容小程序的标签
+		但是html的部分标签在小程序上没有对应标签,所以不推荐使用html标签
+		如果是小程序的标签,在html中,uniapp会实现该标签的所有效果,所以推荐使用小程序标签
+	-->
+	
+	<view class="indexContainer">
+		<view class="header">
+			<image class="logo" src="/static/images/logo.png" mode=""></image>
+			<view class="search">
+				<view class="iconfont icon-sousuo"></view>
+				<input class="searchInput" placeholder="搜索商品" placeholder-class="placeholder" type="text" value="" />
+			</view>
+			<button class="username" type="default">七月</button>
 		</view>
 	</view>
 </template>
 
 <script>
 	export default {
-		data() {
+		data(){
 			return {
 				title: 'Hello1'
+				
 			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
 		}
+		,
+		onLoad() {}
+		,
+		methods: {}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="stylus">
+	// 向后缩进代码是tab,向前缩进是shift+tab
+	.indexContainer
+		.header
+			display flex
+			align-items  center
+			// margin-top 20rpx
+			padding-top 20rpx
+			.logo
+				width 118rpx
+				height 40rpx
+				margin 0 20rpx
+				flex-shrink  0
+			.search
+				position relative
+				height 60rpx
+				background-color  #ccc
+				flex-grow 1
+				border-radius  10rpx
+				padding-left 60rpx
+				.searchInput
+					height 60rpx
+				.iconfont
+					position absolute
+					left 20rpx
+					top 50%
+					transform translateY(-50%)
+				.placeholder
+					font-size 24rpx
+					text-align center
+					line-height 60rpx
+					text-indent -60rpx
+			.username
+				width 140rpx
+				height 60rpx
+				font-size 24rpx
+				margin 0 20rpx
+				flex-shrink  0
+				color red
 </style>
