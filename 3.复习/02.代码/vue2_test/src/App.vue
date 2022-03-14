96b249f5ef3640c4f0827ae10e003d2a64929095
HotJuly <949588380@qq.com>
2022-03-14 16:42:53 +0800

    24.Vue-API(全局配置)

--- 3.复习/02.代码/vue2_test/src/App.vue
+++ 3.复习/02.代码/vue2_test/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ABC/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+// App.vue文件暴露出去的是一个配置对象,根据当前的配置对象Vue会生成组件实例对象
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  count:2,
+  mounted() {
+    // 使用this.$options可以找到生成当前组件实例对象的配置对象
+    // console.log('mounted',this.$options.count)
+    // Object.assign(this.$data,this.$options.data())
+
+    // console.log(c)
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- 3.复习/02.代码/vue2_test/src/main.js
+++ 3.复习/02.代码/vue2_test/src/main.js
@@ -3,6 +3,47 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/*
+  需求:将每个组件配置对象中的count属性,全部加1
+    可以使用自定义合并策略解决该问题
+*/
+
+// Vue.config.optionMergeStrategies.count = function (parent, child, vm) {
+//   // console.log('count',parent, child, vm)
+//   return child * 2
+// }
+
+// 只要以后开发,公司的Vue调试工具不能使用,就执行该代码
+// Vue.config.devtools = true;
+
+/*
+  需求:请问如何捕获到项目中出现的错误?
+  解决:使用try...catch...来捕获出现的错误
+
+  面试题:请问你如何捕获到项目上线之后出现的错误?
+  拆解需求:
+    1.如何捕获到项目中出现的错误?
+      最好使用Vue.config.errorHandler
+
+    2.如何远程获取到用户电脑上出现的报错?
+      只要用户的浏览器报错,就会被流程1进行捕获
+      将用户的错误信息通过ajax发送给公司服务器,最终汇总到BUG平台(TAPD或者禅道等)
+
+  扩展:如果是非框架代码出现报错如何捕获,例如jq项目
+  解决:window.onerror=function(){}
+*/
+
+// Vue.config.errorHandler = function (err, vm, info) {
+//   // err->报错信息
+//   // vm->报错的组件实例对象
+//   // info->报错的位置
+//   console.log('errorHandler',err, vm, info)
+// }'
+
+Vue.config.ignoredElements = [
+  'ABC'
+]
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
