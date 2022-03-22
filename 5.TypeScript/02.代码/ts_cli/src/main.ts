4a297efbc858deb3fce24e5f4a1c355fed488b72
HotJuly <949588380@qq.com>
2022-03-22 12:03:00 +0800

    03.TS基础数据类型

--- 5.TypeScript/02.代码/ts_cli/src/main.ts
+++ 5.TypeScript/02.代码/ts_cli/src/main.ts
@@ -1,5 +1,69 @@
 (function(){
-    let str :string = "12345";
+    let str:string = "12345";
 
-    document.write(str)
+    let boolean:boolean = true;
+    boolean = false;
+    //由于声明变量的时候,已经说明当前变量只能存放布尔值,所以存储数字报错 boolean=123;
+
+    let num:number = 1;
+    num = 0x0012;
+    num = 0b001;
+    num = 0.01;
+
+    // undefined和null在非严格模式下,他们算是所有类型的子类
+    // 在严格模式下,他们就不算任何类型的子类
+    let un:undefined = undefined;
+    // un=1;
+
+    let nu:null = null;
+    // nu=2;
+
+    str=undefined;
+    str=null;
+
+    let arr:Array<number> = [1,2,3];
+    // arr[0]="str";
+
+    let arr2:number[] = [1,2,3];
+
+    // 一下写法就是元组的写法
+    // 元组就是长度和类型都进行了约束的数组
+    let arr3:[number,string,boolean] = [1,"str",true];
+    // arr3[3] = 0;
+    // arr3[1] = "1";
+    // arr3[0].
+
+
+    enum City{
+        "shanghai" = 100,
+        "beijing" = 200,
+        "quanzhou" = 666,
+        "wuhan"
+    }
+
+    
+    let person = {
+        name:"xiaoming",
+        city:City["shanghai"],
+        gender:1
+    }
+
+    // console.log(person,City[person.city])
+    // console.log(City)
+
+    let a:any = true;
+    a = 12;
+    a = "123";
+
+    let a1:string = "123";
+
+    // let v:void = undefined;
+
+    function fn():void{
+        console.log(123)
+    }
+    fn();
+
+    let obj:object = {}
+    // obj = true;
 })();
